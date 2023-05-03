3b9344d9c289433cf6e8b3cfda2114226655988f
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-03 10:07:16 -0700

    fixing a linking problem with each page.

--- js/music.js
+++ js/music.js
@@ -1,3 +1,9 @@
+// let kick = Audio('');
+// let snare = Audio('');
+// let hihat_open = Audio('');
+// let hihat_close = Audio('');
+
+
 let btn4 = document.getElementsByClassName('musicButton');
 
 for(let btn of btn4) {
@@ -6,3 +12,43 @@ for(let btn of btn4) {
         btn.classList.toggle('active_music');
     })
 }
+
+// let btn5 = document.querySelectorAll('kick_btn');
+
+// for (let btn of btn5) {
+//     btn.addEventListener('click', function () {
+//         if (btn.classList.contains('active_music')) {
+//             kick.play()
+//         }
+//     })
+// }
+
+// let btn6 = document.querySelectorAll('snare_btn');
+
+// for (let btn of btn6) {
+//     btn.addEventListener('click', function () {
+//         if (btn.classList.contains('active_music')) {
+//             snare.play()
+//         }
+//     })
+// }
+
+// let btn7 = document.querySelectorAll('hihat_open')
+
+// for (let btn of btn7) {
+//     btn.addEventListener('click', function () {
+//         if (btn.classList.contains('active_music')) {
+//             hihat_open.play()
+//         }
+//     })
+// }
+
+// let btn8 = document.querySelectorAll('hihat_close')
+
+// for (let btn of btn8) {
+//     btn.addEventListener('click', function () {
+//         if (btn.classList.contains('active_music')) {
+//             hihat_close.play()
+//         }
+//     })
+// }
