2b78953d5c0ed7f96d7f210f339292fe173d58d8
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-06 20:34:31 -0700

    Testing some code

--- js/music.js
+++ js/music.js
@@ -1,54 +1,55 @@
-// let kick = Audio('');
-// let snare = Audio('');
-// let hihat_open = Audio('');
-// let hihat_close = Audio('');
+// // let kick = Audio('');
+// // let snare = Audio('');
+// // let hihat_open = Audio('');
+// // let hihat_close = Audio('');
 
 
-let btn4 = document.getElementsByClassName('musicButton');
+// let btn4 = document.getElementsByClassName('musicButton');
 
-for(let btn of btn4) {
-    btn.addEventListener('click', function(){
-        console.log("test")
-        btn.classList.toggle('active_music');
-    })
-}
-
-// let btn5 = document.querySelectorAll('kick_btn');
-
-// for (let btn of btn5) {
-//     btn.addEventListener('click', function () {
-//         if (btn.classList.contains('active_music')) {
-//             kick.play()
-//         }
+// for(let btn of btn4) {
+//     btn.addEventListener('click', function(){
+//         console.log("test")
+//         btn.classList.toggle('active_music');
 //     })
 // }
 
-// let btn6 = document.querySelectorAll('snare_btn');
+// // let btn5 = document.querySelectorAll('kick_btn');
 
-// for (let btn of btn6) {
-//     btn.addEventListener('click', function () {
-//         if (btn.classList.contains('active_music')) {
-//             snare.play()
-//         }
-//     })
-// }
+// // for (let btn of btn5) {
+// //     btn.addEventListener('click', function () {
+// //         if (btn.classList.contains('active_music')) {
+// //             kick.play()
+// //         }
+// //     })
+// // }
 
-// let btn7 = document.querySelectorAll('hihat_open')
+// // let btn6 = document.querySelectorAll('snare_btn');
 
-// for (let btn of btn7) {
-//     btn.addEventListener('click', function () {
-//         if (btn.classList.contains('active_music')) {
-//             hihat_open.play()
-//         }
-//     })
-// }
+// // for (let btn of btn6) {
+// //     btn.addEventListener('click', function () {
+// //         if (btn.classList.contains('active_music')) {
+// //             snare.play()
+// //         }
+// //     })
+// // }
 
-// let btn8 = document.querySelectorAll('hihat_close')
+// // let btn7 = document.querySelectorAll('hihat_open')
 
-// for (let btn of btn8) {
-//     btn.addEventListener('click', function () {
-//         if (btn.classList.contains('active_music')) {
-//             hihat_close.play()
-//         }
-//     })
-// }
+// // for (let btn of btn7) {
+// //     btn.addEventListener('click', function () {
+// //         if (btn.classList.contains('active_music')) {
+// //             hihat_open.play()
+// //         }
+// //     })
+// // }
+
+// // let btn8 = document.querySelectorAll('hihat_close')
+
+// // for (let btn of btn8) {
+// //     btn.addEventListener('click', function () {
+// //         if (btn.classList.contains('active_music')) {
+// //             hihat_close.play()
+// //         }
+// //     })
+// // }
+
