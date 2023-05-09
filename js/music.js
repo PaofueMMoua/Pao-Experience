2c53901939d11de134de3b2dc6ab5fe30ceff9d7
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-09 16:42:42 -0700

    Fixing the Music

--- js/music.js
+++ js/music.js
@@ -12,7 +12,9 @@
 //     console.log('testing')
 // })
 
-const kick = new Audio('/Kick.mp3');
+const kick = new Audio('/Kick.mp3'); 
 const snare = new Audio('/Snare.mp3');
-const hihat_close = new Audio('/Hihat_Close');
-const hihat_open = new Audio('/Hihat_Open');
+const hihat_close = new Audio('/Hihat_Closed.mp3');
+const hihat_open = new Audio('/Hihat_Open.wav');
+
+
