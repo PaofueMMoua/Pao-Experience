e2af7c603ed16e783fd6003bafb7de76a7603b95
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-10 15:32:38 -0700

    Fixing

--- js/script.js
+++ js/script.js
@@ -31,15 +31,15 @@ btn3.addEventListener('click', () => {
     input3.focus()
 })
 
-window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const data = new FormData(form);
-        const action = e.target.action;
-        fetch(action, {
-        method: 'POST',
-        body: data,
-        })
-    });
-});
+// window.addEventListener("load", function() {
+//     const form = document.getElementById('my-form');
+//     form.addEventListener("submit", function(e) {
+//         e.preventDefault();
+//         const data = new FormData(form);
+//         const action = e.target.action;
+//         fetch(action, {
+//         method: 'POST',
+//         body: data,
+//         })
+//     });
+// });
