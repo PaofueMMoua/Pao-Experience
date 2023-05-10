9dce05d57b14be67e5fd132912086d8ba6f1558a
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-10 15:04:42 -0700

    Fixing the Resume

--- js/music.js
+++ js/music.js
@@ -1,20 +1,4 @@
-// const kick = new Audio('.Kick.mp4');
-// const snare = Audio('');
-// const hihat_open = Audio('');
-// const hihat_close = Audio('');
-
-// const kick_btn = document.getElementsByClassName('kick_btn');
-// const snare_btn = document.querySelectorAll('snare_btn');
-// const hihat_open_btn = document.querySelectorAll('hihat_open')
-// const hihat_close_btn = document.querySelectorAll('hihat_close')
-
-// kick_btn.addEventListener('click', function (e) {
-//     console.log('testing')
-// })
-
 const kick = new Audio('/Kick.mp3'); 
 const snare = new Audio('/Snare.mp3');
 const hihat_close = new Audio('/Hihat_Closed.mp3');
 const hihat_open = new Audio('/Hihat_Open.wav');
-
-
