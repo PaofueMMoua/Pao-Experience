a5fe9ed8c169038ed5a1b3bbca08b37923831889
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-05-15 09:24:27 -0700

    Fixing my mistakes lol

--- js/script.js
+++ js/script.js
@@ -36,37 +36,49 @@ let c = 0;
 function random() {
     if (c = 0) {
         alert("why did you click me?")
+        console.log(c)
+        console.log(c)
         c++
     }
     else if (c = 1) {
         alert('Please stop clicking me')
+        console.log(c)
         c++
     }else if (c = 2) {
         alert('Please dont click me again')
+        console.log(c)
         c++
     }else if (c = 3) {
         alert('Why are you doing this ')
+        console.log(c)
         c++
     }else if (c = 4) {
         alert('You know this hurts me when you do this')
+        console.log(c)
         c++
     }else if (c = 5) {
         alert('Why do you still do this')
+        console.log(c)
         c++
     }else if (c = 6) {
         alert('I`m giving up on this ')
+        console.log(c)
         c++
     }else if (c = 7) {
         alert('. . .')
+        console.log(c)
         c++
     }else if (c = 8) {
         alert(' WHYYYYYY')
+        console.log(c)
         c++
     }else if (c = 9) {
         alert('WHY DO YOU DO THIS ARE YOU SOME KINDA DETECTIVE I KNOW NOTHING')
+        console.log(c)
         c++
     }else if (c = 10) {
         alert('I GIVE UP BECAUSE FOR SOME REASON YOU HAVE THE TIME TO SIT THERE AND CLICK THIS ONE BUTTON OVER AND OVER AND OVER AGAIN UNTILL IT JUST ANNOYES ME DONT YOU UNDERSTAND WHAT YOU ARE DOING WITHIN THIS TIME FRAME WHYYY DID YOU JUST WAIST YOUR TIME DOING THIS LIKE WHYYYYY.')
+        console.log(c)
         c = 0
     }
 
